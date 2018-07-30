19462921a7a3d7577de64dceb6449f69e293c8ea
art123craft <cammiebluecammie@gmail.com>
2018-07-30 20:25:42 +0000

    Done.

--- silly_string.rb
+++ silly_string.rb
@@ -1,38 +1,25 @@
-# Challenge 1: Fill this string in with your favorite word
-your_favorite_word ="sweet"
+your_favorite_word = "sweet"
 puts "My favorite word is #{your_favorite_word}"
-
-# Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
 favorite_word_in_all_caps = your_favorite_word.upcase
 puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
-
-# # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
 phrase = "IM NOT SHOUTING"
 lowercased_phrase = phrase.downcase
 puts lowercased_phrase
 
-# # Challenge 4: Count how many letters are in that big_word
-
 big_word = "supercalifragilisticexpialidocious"
 letter_count = big_word.size
 puts "There are #{letter_count} in #{big_word}"
-
-# Challenge 5: Add "wow" and "mom" to big_word
 
 bigger_word = big_word + "wow" + "mom"
 new_letter_count = bigger_word.size
 puts "There are now #{new_letter_count} in #{bigger_word}"
 
-# Challenge 6: Capitalize this sentence properly
-
 proper_sentence = "i really like programming.".capitalize
 puts proper_sentence
 
-# Challenge 7: String Switcheroo
 sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
 
-# Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 sentence = sentence.gsub("a","o").gsub("A","O")
 puts sentence
--- what_your_name.rb
+++ what_your_name.rb
@@ -1,2 +1,23 @@
-puts "What is your name?"
-gets 
+# class ClassName
+#   def method_name(parameter)
+#     @class_variable = parameter
+#   end
+# end
+
+# class Dog
+  
+#   def initialize(name, breed)
+#     @name = name
+#     @breed = breed
+#   end
+  
+#   public
+#   def bark
+#     puts "woof!"
+#   end
+
+# private
+# def id
+#   @id_number = 12345
+#   end
+# end
